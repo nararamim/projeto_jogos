b56ad9d07e2ef32cdee405190866863810a80d05
Tainara <nara.ramim@gmail.com>
2018-10-14 10:46:24 -0300

    Criando o projeto de jogos

---
+++ desktop/src/br/edu/ufabc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.edu.ufabc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.edu.ufabc.JogoProjeto;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JogoProjeto(), config);
+	}
+}
