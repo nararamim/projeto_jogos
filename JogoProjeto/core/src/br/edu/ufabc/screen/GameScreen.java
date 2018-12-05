56d43e3f004fc3e68744656071828e83a08df44e
pkeiko <paula.keiko.m@gmail.com>
2018-12-04 23:45:32 -0200

    tentativa de loop de obstaculos

--- JogoProjeto/core/src/br/edu/ufabc/screen/GameScreen.java
+++ JogoProjeto/core/src/br/edu/ufabc/screen/GameScreen.java
@@ -48,56 +48,149 @@ public class GameScreen extends AbstractScreen{
 	private BitmapFont 					font;
 	private SpriteBatch 				spriteBatch;
 	private Matrix4 					viewMatrix;
-	private float						zcaminho;
 	private float						countcaminho;
     private boolean                     trombou;
     private float						speed = 3f;
     private Music           			gameMusic;
+    private float 						countobstaculo;
     
 	//////////////////////////////////////////////////
 
 	public GameScreen(String id, int maxScore) {
 		super(id);
-		modelBatch = new ModelBatch();
-
-        setEnvironment();
-        //set array de Caminhos
-        caminhos = new ArrayList<GameObject>();
-        setCamera();
-
-        ModelLoader<ModelLoader.ModelParameters> loader;
-		loader = new G3dModelLoader(new UBJsonReader());
-
-        setCeu(loader);
 		
-		Bart = new Bart();
-        objetos = new ArrayList<GameObject>();
-
-        // TODO: adicionar demais objetos
-        for (GameObject n : caminhos) {
-            GameObject rato = new GameObject(ModelFactory.getModelbyName("rato"));
-            Vector3 pos = new Vector3();
-            //n.transform.getTranslation(pos);
-            rato.transform.translate(0,0,0);
-            rato.update(1);
-            objetos.add(rato);
-        }
-
-        setFonts();
-
-        spriteBatch = new SpriteBatch();
-		viewMatrix = new Matrix4();
-		zcaminho = -80;
-		countcaminho = 0;
-		
-		setMaxScore(maxScore);
-
-        setupAlternanciaCaminhos();
+		ModelLoader<ModelLoader.ModelParameters> loader;
+		modelBatch = 	new ModelBatch();
+		caminhos = 		new ArrayList<GameObject>();
+		loader = 		new G3dModelLoader(new UBJsonReader());
+		Bart = 			new Bart();
+        objetos = 		new ArrayList<GameObject>();
+        spriteBatch = 	new SpriteBatch();
+		viewMatrix = 	new Matrix4();
+		countcaminho = 	0;
+		countobstaculo= 0;
         
+        setEnvironment();
+        setCamera();
+        setCeu(loader);
+        setFonts();
+        setMaxScore(maxScore);
+        setupAlternanciaCaminhos();
+        addObstaculo();
         musicInitialization();
+		        
+        
+        // TODO: adicionar demais objetos
+        /*for (int i = 0 ; i < 20; i ++) {
+            GameObject objeto = new GameObject(ModelFactory.getModelbyName("rato"));
+            Vector3 pos = new Vector3();
+            pos.x = 0;
+            pos.y = 0;
+            pos.z = zobj;
+            zobj-=190;
+            objeto.transform.translate(pos);
+            //objeto.transform.scale(0.01f, 0.01f, 0.01f );
+            objeto.update(1);
+            objetos.add(objeto);
+        }		*/
 	}
 
+    @Override
+	public void dispose() {
+		modelBatch.dispose();
+	}
+
+	@Override
+	public void update(float delta) {
+		countcaminho+=speed;
+		countobstaculo+=speed;
+		pontos+=0.01*speed;
+		if (Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
+			if ((int) pontos > getMaxScore()) {
+				setMaxScore((int)pontos);
+			}		
+			gameMusic.dispose();
+			setDone(true);
+		}
+		if (vidas > 0) {
+	        updateCaminho();	
+	        for(GameObject g : caminhos) {	        	
+	        	g.transform.translate(0,0,speed);
+				g.update(1);        	
+			}
+			updateObstaculos();
+	        for(GameObject h: objetos) {
+	        	System.out.println(objetos.size());
+	        	h.transform.translate(0,0,speed);
+	        	h.update(1);
+	        }	        
+		}
+		else {
+			if ((int) pontos > getMaxScore()) setMaxScore((int)pontos);
+			Bart.morrer();
+			Bart.update(delta);
+			gameMusic.dispose();
+			setDone(true);
+		}
+
+		// teclas de jogo
+		if(Gdx.input.isKeyJustPressed(Keys.UP) || Gdx.input.isButtonPressed(Buttons.MIDDLE)) Bart.pular();
+		if(Gdx.input.isKeyJustPressed(Keys.RIGHT) || Gdx.input.isButtonPressed(Buttons.RIGHT)) Bart.direita();
+		if(Gdx.input.isKeyJustPressed(Keys.LEFT) || Gdx.input.isButtonPressed(Buttons.LEFT)) Bart.esquerda();
+        if(Gdx.input.isKeyJustPressed(Keys.SPACE)) {
+        	vidas--;
+            Bart.morrer();
+        }		
+		Bart.update(delta);
+        checkColisions();
+	}
+
+    @Override
+	public void draw(float delta) {
+        // TODO Auto-generated method stub
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		
+		modelBatch.begin(camera);
+		modelBatch.render(Bart.getCurrent(), environment);
+		
+		for(GameObject g : caminhos) modelBatch.render(g,environment);
+		for(GameObject h : objetos) modelBatch.render(h,environment);
+		for(ModelInstance m: ceus) modelBatch.render(m, environment);
+		
+		
+		modelBatch.end();
+		
+		camera.update();
+		///////////////////////////////////////
+		viewMatrix.setToOrtho2D(0, 0, Parameters.GAME_WIDTH, Parameters.GAME_HEIGHT);
+		spriteBatch.setProjectionMatrix(viewMatrix);
+		spriteBatch.begin();
+		//pontos+=1;
+		font.draw(spriteBatch, "Score\n" + (int) pontos, 10, 550);
+		font.draw(spriteBatch, "MaxScore\n" + (int) getMaxScore(), 330, 550);
+		font.draw(spriteBatch, "Lifes\n" + (int) vidas, 670, 550);
+		spriteBatch.end();
+		
+		///////////////////////////////////////
+	}
+
+    public void checkColisions() {
+        for (GameObject g : objetos) {
+            if (g.collidesWith(Bart.getCurrent())) {
+                //System.out.println("Bart trombou no objeto");
+                trombou = true;
+                //vidas--;
+                break;
+            } else {
+                trombou = false;
+            }
+        }
+    }
+
     private void setupAlternanciaCaminhos() {
+		int zcaminho = 		-80;
         for(int i = 0; i < 5; i++) {
             GameObject caminho = new GameObject(ModelFactory.getModelbyName("chao"));
             Vector3 pos = new Vector3();
@@ -110,6 +203,23 @@ public class GameScreen extends AbstractScreen{
 
             caminhos.add(caminho);
         }
+    }
+    
+    private void addObstaculo() {
+        //int zobj = 100;
+        int zobj = -80;
+        for (int i = 0 ; i < 3; i ++) {
+            GameObject objeto = new GameObject(ModelFactory.getModelbyName("rato"));
+            Vector3 pos = new Vector3();
+            pos.x = 0;
+            pos.y = 0;
+            pos.z = zobj;
+            zobj-=190;
+            objeto.transform.translate(pos);
+            objeto.transform.scale(0.01f, 0.01f, 0.01f );
+            objeto.update(1);
+            objetos.add(objeto);
+        }		
     }
     
     private void musicInitialization() {
@@ -151,61 +261,7 @@ public class GameScreen extends AbstractScreen{
         camera.far  = 800f;
         camera.update();
     }
-
-    @Override
-	public void dispose() {
-		modelBatch.dispose();
-	}
-
-	@Override
-	public void update(float delta) {
-		countcaminho+=speed;
-		pontos+=0.01*speed;
-		if (Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
-			if ((int) pontos > getMaxScore()) {
-				setMaxScore((int)pontos);
-			}		
-			gameMusic.dispose();
-			setDone(true);
-		}
-
-		if (vidas > 0) {
-	        updateCaminho();
-	
-	        for(GameObject g : caminhos) {	        	
-					g.transform.translate(0,0,speed);
-					g.update(1);        	
-			}		
-		}
-		else {
-			if ((int) pontos > getMaxScore()) {
-				setMaxScore((int)pontos);
-			}
-			Bart.morrer();
-			Bart.update(delta);
-			gameMusic.dispose();
-			setDone(true);
-		}
-
-		// teclas de jogo
-		if(Gdx.input.isKeyJustPressed(Keys.UP) || Gdx.input.isButtonPressed(Buttons.MIDDLE)) {
-			Bart.pular();
-		}
-		if(Gdx.input.isKeyJustPressed(Keys.RIGHT) || Gdx.input.isButtonPressed(Buttons.RIGHT)) {
-			Bart.direita();
-		}
-		if(Gdx.input.isKeyJustPressed(Keys.LEFT) || Gdx.input.isButtonPressed(Buttons.LEFT)) {
-			Bart.esquerda();
-		}
-        if(Gdx.input.isKeyJustPressed(Keys.SPACE)) {
-        	vidas--;
-            Bart.morrer();
-        }
-		
-		Bart.update(delta);
-        checkColisions();
-	}
-
+    
     private void updateCaminho() {
         if(countcaminho >= 190) {
             GameObject caminho = new GameObject(ModelFactory.getModelbyName("chao"));
@@ -215,56 +271,26 @@ public class GameScreen extends AbstractScreen{
             pos.z = -850;
             caminho.transform.translate(pos);
             caminho.update(1);
-
             caminhos.add(caminho);
             countcaminho-= 190;
             caminhos.remove(0);
         }
         if(speed < 6.5) speed+=0.0005;
     }
-
-    @Override
-	public void draw(float delta) {
-        // TODO Auto-generated method stub
-		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		
-		modelBatch.begin(camera);
-		modelBatch.render(Bart.getCurrent(), environment);
-		
-		for(GameObject g : caminhos) {
-			modelBatch.render(g,environment);
-		}
-		for(ModelInstance m: ceus) {
-			modelBatch.render(m, environment);
-		}
-		modelBatch.end();
-		
-		camera.update();
-		///////////////////////////////////////
-		viewMatrix.setToOrtho2D(0, 0, Parameters.GAME_WIDTH, Parameters.GAME_HEIGHT);
-		spriteBatch.setProjectionMatrix(viewMatrix);
-		spriteBatch.begin();
-		//pontos+=1;
-		font.draw(spriteBatch, "Score\n" + (int) pontos, 10, 550);
-		font.draw(spriteBatch, "MaxScore\n" + (int) getMaxScore(), 330, 550);
-		font.draw(spriteBatch, "Lifes\n" + (int) vidas, 670, 550);
-		spriteBatch.end();
-		
-		///////////////////////////////////////
-	}
-
-    public void checkColisions() {
-        for (GameObject g : objetos) {
-            if (g.collidesWith(Bart.getCurrent())) {
-                System.out.println("Bart trombou no objeto");
-                trombou = true;
-                vidas--;
-                break;
-            } else {
-                trombou = false;
-            }
-        }
+    
+    private void updateObstaculos() {
+    	if(countobstaculo >= 190) {
+    		GameObject objeto = new GameObject(ModelFactory.getModelbyName("rato"));
+    		Vector3 pos = new Vector3();
+            pos.x = 0;
+            pos.y = 0;
+            pos.z = -600;
+            objeto.transform.translate(pos);
+            objeto.transform.scale(0.01f, 0.01f, 0.01f );
+            objeto.update(1);
+            objetos.add(objeto);
+            countobstaculo-=190;
+            objetos.remove(0);    		
+    	}
     }
 }
