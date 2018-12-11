7ca1f3b3a5b3824936b84bceebe0215f973fcad3
pkeiko <paula.keiko.m@gmail.com>
2018-12-11 01:00:31 -0200

    pulo

--- JogoProjeto/core/src/br/edu/ufabc/model/Bart.java
+++ JogoProjeto/core/src/br/edu/ufabc/model/Bart.java
@@ -15,7 +15,10 @@ public class Bart {
 	boolean updatingcolunadir = false;
 	boolean updatingcolunaesq = false;
 	private float moving = 0;
-	private float jumping = 0;
+	private int jumping = 0;
+	private boolean sobe = false;
+	private boolean change = false;
+
 
 	public Bart() {
 		estados = new GameObject[3];
@@ -45,14 +48,31 @@ public class Bart {
 		if (estado == JUMP && estados[estado].isDone()) {
 			estados[estado].reset();
 			estado = RUN;
+		} else if (estado == JUMP) {
+			if (sobe) {
+				jumping++;
+				for (GameObject estado : estados)
+					estado.transform.translate(0, 0.35f, 0);
+				if (jumping > 20)
+					sobe = false;
+			} else {
+				if (jumping >= 0) {
+					for (GameObject estado : estados)
+						estado.transform.translate(0, -0.35f, 0);
+					jumping--;
+				}
+			}
 		}
-
 		if (estado == DYING && estados[estado].isDone()) {
 			setEndGame(true);
 		}
 
 		if (updatingcolunadir) {
 			if (moving < 25) {
+				if(moving == 8.0f && change) {
+					posicao++;
+					change = false;
+				}
 				moving += 0.51f;
 				for (GameObject estado : estados)
 					estado.transform.translate(-0.51f, 0, 0);
@@ -63,6 +83,10 @@ public class Bart {
 		}
 		if (updatingcolunaesq) {
 			if (moving < 25) {
+				if(moving ==8.0f && change) {
+					posicao--;
+					change = false;
+				}
 				moving += 0.51f;
 				for (GameObject estado : estados)
 					estado.transform.translate(0.51f, 0, 0);
@@ -74,9 +98,10 @@ public class Bart {
 	}
 
 	public void pular() {
-		// TODO Lógica de pular em parábola
-		estado = JUMP;
-		jumping = 0.05f;
+		if (estado != JUMP) {
+			estado = JUMP;
+			sobe = true;
+		}
 	}
 
 	public void morrer() {
@@ -89,8 +114,8 @@ public class Bart {
 
 			if (posicao < 1) {
 				updatingcolunadir = true;
-				posicao++;
 				estado = JUMP;
+				change = true;
 			} else {
 				posicao = 1;
 			}
@@ -104,8 +129,8 @@ public class Bart {
 
 			if (posicao > -1) {
 				updatingcolunaesq = true;
-				posicao--;
 				estado = JUMP;
+				change = true;
 			} else {
 				posicao = -1;
 			}
@@ -130,5 +155,9 @@ public class Bart {
 
 	public int getPosicao() {
 		return this.posicao;
+	}
+	
+	public int getAltura() {
+		return this.jumping;
 	}
 }
